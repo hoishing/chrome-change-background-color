e9e78fac5ec5fe8af898bfa28c553ed5ae5ffaa1
Kelvin Ng <hoishing@gmail.com>
2026-01-05 12:16:27 +0800

    add change all element bg option

--- src/popup.js
+++ src/popup.js
@@ -1,28 +1,35 @@
-document.getElementById("colorPicker").addEventListener("input", event => {
-  changeBackground(event.target.value)
-})
+document.getElementById("colorPicker").addEventListener("input", (event) => {
+  changeBackground(event.target.value);
+});
 
-const colors = ["#fffaec", "#f8f3e7", "#d9d9d9", "#bdbdbd"]
+const colors = ["#fffaec", "#f8f3e7", "#d9d9d9", "#bdbdbd"];
 
 for (var i = 0; i < 4; i++) {
-  let ele = document.getElementById(`color${i}`)
-  let color = colors[i]
+  let ele = document.getElementById(`color${i}`);
+  let color = colors[i];
   ele.addEventListener("click", () => {
-    changeBackground(color)
-  })
-  ele.style.backgroundColor = color
+    changeBackground(color);
+  });
+  ele.style.backgroundColor = color;
 }
 
 function changeBackground(color) {
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const scope = document.querySelector('input[name="colorScope"]:checked').value;
+    const func = scope === "body" ? changeBackgroundColor : changeAllElementsBackgroundColor;
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
-      function: changeBackgroundColor,
+      function: func,
       args: [color],
-    })
-  })
+    });
+  });
 }
 
 function changeBackgroundColor(color) {
-  document.body.style.backgroundColor = color
+  document.body.style.backgroundColor = color;
+}
+
+function changeAllElementsBackgroundColor(color) {
+  const sheet = window.document.styleSheets[0];
+  sheet.insertRule("* { background-color: " + color + " !important; }", sheet.cssRules.length);
 }
